99baadf582bdb7c75dedc1aa5f4c0228c358cffd
Leonardo <leonardofranca.carvalho@gmail.com>
2015-11-20 21:37:41 -0200

    aaa.

--- estoque/src/br/alef/controller/UsuarioBean.java
+++ estoque/src/br/alef/controller/UsuarioBean.java
@@ -1,12 +1,15 @@
 package br.alef.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 
+import br.alef.model.Usuario;
 import br.alef.model.UsuarioDAO;
 
 @ManagedBean
@@ -18,6 +21,13 @@ public class UsuarioBean implements Serializable {
 	private String email;
 	private String nome;
 	private String senha;
+	private Usuario usuario;
+	private List<Usuario> usuarios;
+
+	@PostConstruct
+	public void init() {
+		usuario = new Usuario();
+	}
 
 	public void cadastrarUsuario() {
 		UsuarioDAO.inserir(nome, email, senha);
@@ -27,6 +37,14 @@ public class UsuarioBean implements Serializable {
 						"Usuário cadastrado com sucesso!!", null));
 		email = null;
 		nome = null;
+	}
+
+	public void pesquisarUsuarios() {
+		usuarios = UsuarioDAO.pesquisarUsuarios(usuario);
+	}
+
+	public void limparDados() {
+
 	}
 
 	public String getEmail() {
@@ -51,6 +69,22 @@ public class UsuarioBean implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
 	}
 
 }
--- estoque/src/br/alef/model/UsuarioDAO.java
+++ estoque/src/br/alef/model/UsuarioDAO.java
@@ -2,7 +2,10 @@ package br.alef.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
 
@@ -18,9 +21,41 @@ public class UsuarioDAO {
 			ps.setString(3, senha);
 
 			ps.executeUpdate();
-			
+
 		} catch (SQLException ex) {
 			ex.printStackTrace();
+		} finally {
+			ConexaoBanco.fecharConexao(con);
+		}
+	}
+
+	public static List<Usuario> pesquisarUsuarios(Usuario usuario) {
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = ConexaoBanco.obterConexao();
+			ps = con.prepareStatement("select * from usuario where nome LIKE ?");
+			String nome = "";
+			if (usuario.getNome() != null) {
+				nome = usuario.getNome();
+			}
+			ps.setString(1, "%" + nome + "%");
+
+			ResultSet rs = ps.executeQuery();
+			List<Usuario> usuarios = new ArrayList<Usuario>();
+			while (rs.next()) {
+				Usuario usuarioLocal = new Usuario();
+				usuarioLocal.setNome(rs.getString("nome"));
+				usuarioLocal.setEmail(rs.getString("email"));
+				usuarioLocal.setId(rs.getInt("id"));
+				usuarioLocal.setSenha(rs.getString("senha"));
+				usuarios.add(usuarioLocal);
+			}
+			return usuarios;
+		} catch (SQLException ex) {
+			System.out.println("Login error -->" + ex.getMessage());
+			return null;
 		} finally {
 			ConexaoBanco.fecharConexao(con);
 		}
